22fcca03acfe9c3235120fca2154120203f48495
Joel Vinueza <jvinuezag1@est.ups.edu.ec>
2024-06-11 00:55:09 -0500

    Especificar Nombre a la base

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 import { config } from "dotenv";
 config();
 
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://Difare:10100@cluster0.uk8qaja.mongodb.net/";
+export const dbName = process.env.dbName || "InventarioGD";
+export const MONGODB_URI = 
+process.env.MONGODB_URI || `mongodb+srv://Difare:10100@cluster0.uk8qaja.mongodb.net/${ dbName }`;
 export const PORT = process.env.PORT || 4000;
+
 export const SECRET = "D1f@r3S3cr3tK3y";
 
 export const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@grupodifare.com";
