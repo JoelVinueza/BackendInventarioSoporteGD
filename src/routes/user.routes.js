a82ccf5786e248354cc3ef154c21dad77705fc4a
Joel Vinueza <jvinuezag1@est.ups.edu.ec>
2024-06-12 18:01:11 -0500

    Mandar Users y byId

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { getUsers, createUser } from "../controllers/user.controller.js";
 import { isAdmin, verifyToken } from "../middlewares/authJwt.js";
 import { checkExistingUser } from "../middlewares/verifySignup.js";
 
 const router = Router();
 
+router.get("/", getUsers);
+router.get("/:userId", getUserByID);
 router.post("/", [verifyToken, isAdmin, checkExistingUser], createUser);
 
 export default router;
